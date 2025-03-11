7614ba23ce57e931fe07ef1a5a54ee072e6669c4
Bernard Gresola <btgresola@uerm.edu.ph>
2025-03-11 16:16:22 +0800

    Update quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -46,9 +46,9 @@ module.exports = configure(function (ctx) {
       vueRouterMode: "hash", // available values: 'hash', 'history'
       env: {
         // API_HOST: "https://jsonplaceholder.typicode.com",
-        API_HOST: "https://48ae-202-57-35-84.ngrok-free.app",
-        // API_HOST: "http://localhost:8888",
-        NGROK_HOST: "https://3325-202-57-35-84.ngrok-free.app",
+        // API_HOST: "https://48ae-202-57-35-84.ngrok-free.app",
+        API_HOST: "http://localhost:8888",
+        NGROK_HOST: "https://48ae-202-57-35-84.ngrok-free.app",
         // DEV_HOST: "https://api.uerm.edu.ph/dragonpay", // AT HOME
         // DEV_HOST: " https://b84c-202-57-35-84.ngrok-free.app/dragonpay", //NGROK
         DEV_HOST: "http://localhost:8888",
